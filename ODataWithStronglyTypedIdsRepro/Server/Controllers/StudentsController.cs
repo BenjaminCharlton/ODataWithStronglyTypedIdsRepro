01370efe647b289caf12d16868e4c5e0850c730f
Benjamin Charlton <benjaminjcharlton@gmail.com>
2020-04-11 21:34:41 +0400

    The Controller was generating StudentIds using StudentId.New() just for illustrative purposes but, as it wasn't realistic, I simulated actual data by calling new StudentId(Guid guid) with a hardcoded guid.

--- ODataWithStronglyTypedIdsRepro/Server/Controllers/StudentsController.cs
+++ ODataWithStronglyTypedIdsRepro/Server/Controllers/StudentsController.cs
@@ -23,11 +23,15 @@ namespace ODataWithStronglyTypedIdsRepro.Server.Controllers
         [EnableQuery]
         public IEnumerable<ViewStudentDto> Get()
         {
+            /* In reality, the controller will get this list from the service tier, and the service
+             * tier will get it from the data tier, and the data tier (which will be entity framework)
+             will get it from the database. All that is too complex to reproduce in this sample app
+             so I'm just going to hard code the list here. */
             IList<ViewStudentDto> list = new List<ViewStudentDto>();
 
             list.Add(new ViewStudentDto
             {
-                Id = StudentId.New(),
+                Id = new StudentId(new Guid("54c361f2-6e83-4b1e-9dd1-badb736bb493")),
                 Name = "Boris",
                 WhenEnrolled = new DateTime(2018, 08, 30)
 
@@ -35,7 +39,7 @@ namespace ODataWithStronglyTypedIdsRepro.Server.Controllers
 
             list.Add(new ViewStudentDto
             {
-                Id = StudentId.New(),
+                Id = new StudentId(new Guid("66159e11-97ae-4be7-894d-a1165dd5e5eb")),
                 Name = "Katharina",
                 WhenEnrolled = new DateTime(2019, 09, 3)
 
@@ -43,7 +47,7 @@ namespace ODataWithStronglyTypedIdsRepro.Server.Controllers
 
             list.Add(new ViewStudentDto
             {
-                Id = StudentId.New(),
+                Id = new StudentId(new Guid("16417233-daef-4a0f-ac4a-b72a1dbdf2eb")),
                 Name = "Erika",
                 WhenEnrolled = new DateTime(2018, 08, 28)
 
@@ -51,7 +55,7 @@ namespace ODataWithStronglyTypedIdsRepro.Server.Controllers
 
             list.Add(new ViewStudentDto
             {
-                Id = StudentId.New(),
+                Id = new StudentId(new Guid("83647ea9-f063-4707-ab97-4b881cae4822")),
                 Name = "Sergei",
                 WhenEnrolled = new DateTime(2019, 10, 1)
 
