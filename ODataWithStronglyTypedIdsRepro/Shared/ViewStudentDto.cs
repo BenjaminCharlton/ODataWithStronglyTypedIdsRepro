fd0afb3ce74d365027e47007eb5c4f17d8fbf7e7
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2020-04-11 12:15:32 +0200

    Fix EDM and Json serialization

--- ODataWithStronglyTypedIdsRepro/Shared/ViewStudentDto.cs
+++ ODataWithStronglyTypedIdsRepro/Shared/ViewStudentDto.cs
@@ -1,14 +1,18 @@
 ﻿using ODataWithStronglyTypedIdsRepro.Shared.ValueObjects;
 using System;
+using System.Text.Json.Serialization;
 
 namespace ODataWithStronglyTypedIdsRepro.Shared
 {
-    public class ViewStudentDto
-    {
-        public StudentId Id { get; set; }
+public class ViewStudentDto
+{
+    public Guid Id { get => StudentId.Value; set => StudentId = new StudentId(value); }
+        
+    [JsonIgnore]
+    public StudentId StudentId { get; set; }
 
-        public DateTime WhenEnrolled { get; set; }
+    public DateTime WhenEnrolled { get; set; }
 
-        public string Name { get; set; }
-    }
+    public string Name { get; set; }
+}
 }
